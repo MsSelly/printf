128e64894eec2e42c83a9a6127e73ad69b10950f
MsSelly <syskhah@gmail.com>
2023-06-01 10:17:22 -0700

    update

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes c char in stdout
+ * _putchar - This writes c char in stdout
  * @c: the char to print
- * Return: success 1
+ * Return: On success 1
  * On error, return -1 and set error appropriately
  */
 int _putchar(char c)
